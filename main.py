a0676169ff454101cb332f6b2fc46880d9f1fc8b
Durva <durva.chhabria@gmail.com>
2026-02-09 09:15:27 +0530

    Integrated all feature modules

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from add import add
+from sub import sub
+from mul import mul
+
 def main():
+    a = 10
+    b = 5
+
     print("Calculator Application")
+    print("Addition:", add(a, b))
+    print("Subtraction:", sub(a, b))
+    print("Multiplication:", mul(a, b))
 
 if __name__ == "__main__":
     main()
